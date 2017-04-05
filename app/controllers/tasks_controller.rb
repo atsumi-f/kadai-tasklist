c7c7f7a622cebb703d4ca4a93480ec7bd20d922d
atsumi-f <kamaitachi8489@gmail.com>
2017-04-05 14:53:43 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(10)
+   @tasks = Task.all.page(params[:page]).per(10)
   end
   
   def show
@@ -13,14 +14,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスクが作成されませんでした'
-      render :new
+      render '/'
     end
   end
   
@@ -40,7 +41,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to "/"
   end
   
   private
@@ -51,6 +52,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_path
+      end
   end
 end
 
